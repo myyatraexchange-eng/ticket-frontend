c71962f7cb7136f70679e6f03917923f8ccf6a59
myyatraexchange-eng <myyatraexchange@gmail.com>
2025-09-02 09:40:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
-  build: {
-    outDir: 'dist',   // Render यही folder serve करेगा
-  }
+  base: "/", // important for Render
 });
