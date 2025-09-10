7af6f177bcd62dc21c4c50a93c727f72ca0a1ff7
myyatraexchange-eng <myyatraexchange@gmail.com>
2025-09-10 15:21:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // tailwind styles
+import "./index.css";
+import { HelmetProvider } from "react-helmet-async";   // ✅ import
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>   {/* ✅ wrap */}
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
