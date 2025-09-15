f805f7e38a62604cef7600ab667f34bc63d999e0
myyatraexchange-eng <myyatraexchange@gmail.com>
2025-09-15 14:22:13 +0530

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
@@ -29,4 +29,6 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
