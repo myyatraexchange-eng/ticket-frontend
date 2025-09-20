ecca1fa57ab3bae2ac8968f43b21e1ff81209786
myyatraexchange-eng <myyatraexchange@gmail.com>
2025-09-20 12:43:33 +0530

    Update Loader: transparent background, centered spinner and message

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,15 +1,14 @@
 // Loader.js
 import React from "react";
 
-const Loader = ({ message = "Please wait...", size = 16, color = "blue-600" }) => {
+const Loader = ({ message = "Please wait..." }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 pointer-events-none">
-      <div className="flex flex-col items-center">
-        <div
-          className={`loader border-t-4 border-${color} border-solid rounded-full w-${size} h-${size} animate-spin mb-4`}
-        ></div>
-        <p className="text-black text-lg">{message}</p>
-      </div>
+    <div className="fixed inset-0 bg-blue-600 flex flex-col items-center justify-center z-50">
+      {/* Spinner */}
+      <div className="border-4 border-t-white border-white border-solid rounded-full w-16 h-16 animate-spin mb-4"></div>
+
+      {/* Message */}
+      <p className="text-white text-lg font-semibold text-center">{message}</p>
     </div>
   );
 };
