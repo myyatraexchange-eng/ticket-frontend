cf996f40ef671eb474d272c39b34b5b4de32aeaf
myyatraexchange-eng <myyatraexchange@gmail.com>
2025-09-22 17:21:46 +0530

    Fix RefundPolicy.jsx to be deploy-ready

--- src/pages/RefundPolicy.jsx
+++ src/pages/RefundPolicy.jsx
@@ -17,20 +17,14 @@
       margin: auto;
       background: #fff;
       padding: 20px;
-      border-radius: 10px;
-      box-shadow: 0 0 10px rgba(0,0,0,0.1);
+      border-radius: 5px;
     }
     h1 {
       text-align: center;
-      color: #2c3e50;
     }
     ul {
       list-style-type: disc;
       padding-left: 20px;
-    }
-    .highlight {
-      font-weight: bold;
-      color: #e74c3c;
     }
   </style>
 </head>
@@ -39,8 +33,8 @@
     <h1>Refund & Cancellation Policy</h1>
     <p>This refund and cancellation policy outlines how you can cancel or seek a refund for a product/service purchased through our platform.</p>
     <ul>
-      <li>Cancellations will only be considered if the request is made within <span class="highlight">7 days</span>.</li>
-      <li>In case of any refunds approved by <span class="highlight">ROHIT AKODIYA</span>, it will take <span class="highlight">2 days</span> for the refund to be credited to you.</li>
+      <li>Cancellations will only be considered if the request is made within 7 days.</li>
+      <li>In case of any refunds approved by ROHIT AKODIYA, it will take 2 days for the refund to be credited to you.</li>
     </ul>
   </div>
 </body>
